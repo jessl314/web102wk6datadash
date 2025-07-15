0dc013e8f567cc7df0c1da4e66116427d20acf66
jessl314 <jlchrysanthemum456@gmail.com>
2025-07-14 18:59:27 -0700

    added a couple of component files and fixed my nav bar

--- cooperhewittdata/src/App.jsx
+++ cooperhewittdata/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 import NavContainer from './components/NavContainer.jsx'
+// About: switch to a new tab with project description
 
 function App() {
 
   return (
     <>
+    <NavContainer/>
      
     </>
   )
